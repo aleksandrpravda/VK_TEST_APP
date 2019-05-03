7293644c3eb4db1d2ff0b09e056343a48270ec8b
Alexander Rogachev <>
2019-05-03 16:09:35 +0300

    Progect files.

---
+++ VK_TEST_APP/VK_TEST_APP/ViewController.h
@@ -0,0 +1,11 @@
+//
+// Copyright © 2019 Alexander Rogachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VK_TEST_APP/VK_TEST_APP/ViewController.m
@@ -0,0 +1,19 @@
+//
+// Copyright © 2019 Alexander Rogachev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
